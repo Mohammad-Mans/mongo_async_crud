ebc6bef1ac9d1a474b6da86b171818ed4b18ac0d
Mohammad Mansour <mohammed.i02@hotmail.com>
2025-04-10 16:11:31 +0300

    Change allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://auth-with-jwt-eight.vercel.app/",
+  "https://auth-with-jwt-eight.vercel.app",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
